dda3a39d9dbaadff7ebf604b5bdccec5ddd42a31
LordAnthrax94 <andreagi1994@gmail.com>
2025-06-16 15:54:20 +0200

    Fix: inserito file type.ts

---
+++ type.ts
@@ -0,0 +1,13 @@
+export type Videogames = {
+  title: string,
+  category: string,
+  genre: string,
+  platform: string
+  releaseDate: string,
+  developer: string,
+  publisher: string,
+  rating: number,
+  description: string,
+  imageUrl: string,
+  price: number,
+}
